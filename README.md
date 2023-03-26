4ca5ac74df0cba6a522d073ad52c255dbe660c98
michaelKurowski <Byamarro@gmail.com>
2023-03-26 16:57:18 +0300

    cleanup

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-This is an execution of an idea for code reduction, using the univalence principle from homotopy type theory as an inspiration.
+This is a proof of concept of an idea that I've had, using the univalence principle from homotopy type theory as an inspiration.
 
 The general idea is that we define mappers between types to create generic functions that can handle any type that's equivalent to another type.
-
 
 Please see `demo` for example.
 
--- demo/index.ts
+++ demo/index.ts
@@ -21,6 +21,31 @@ type PrettyCustomerRecord = {
   emailAddress: string
 }
 
+graph.addTypeDefinition({
+  name: 'CustomerArr',
+  test(value: unknown): value is CustomerArr {
+    if (!Array.isArray(value) || value.length !== 3) return false
+    return value.every((element: unknown) => typeof element === 'string')
+  }
+})
+
+graph.addTypeDefinition({
+  name: 'PrettyCustomerRecord',
+  test(value: unknown): value is PrettyCustomerRecord {
+    if (typeof value !== 'object' || !value) return false
+    const assumedValue = value as PrettyCustomerRecord
+    return typeof assumedValue.firstName === 'string' && typeof assumedValue.lastName === 'string' && typeof assumedValue.emailAddress === 'string'
+  }
+})
+
+graph.addTypeDefinition({
+  name: 'CustomerRecord',
+  test(value: unknown): value is CustomerRecord {
+    if (typeof value !== 'object' || !value) return false
+    const assumedValue = value as CustomerRecord
+    return typeof assumedValue.name === 'string' && typeof assumedValue.surname === 'string' && typeof assumedValue.email === 'string'
+  }
+})
 
 graph.addMapping({
   fromName: 'CustomerArr',
@@ -48,31 +73,7 @@ graph.addMapping({
   }
 })
 
-graph.addTypeDefinition({
-  name: 'CustomerArr',
-  test(value: unknown): value is CustomerArr {
-    if (!Array.isArray(value) || value.length !== 3) return false
-    return value.every((element: unknown) => typeof element === 'string')
-  }
-})
 
-graph.addTypeDefinition({
-  name: 'PrettyCustomerRecord',
-  test(value: unknown): value is PrettyCustomerRecord {
-    if (typeof value !== 'object' || !value) return false
-    const assumedValue = value as PrettyCustomerRecord
-    return typeof assumedValue.firstName === 'string' && typeof assumedValue.lastName === 'string' && typeof assumedValue.emailAddress === 'string'
-  }
-})
-
-graph.addTypeDefinition({
-  name: 'CustomerRecord',
-  test(value: unknown): value is CustomerRecord {
-    if (typeof value !== 'object' || !value) return false
-    const assumedValue = value as CustomerRecord
-    return typeof assumedValue.name === 'string' && typeof assumedValue.surname === 'string' && typeof assumedValue.email === 'string'
-  }
-})
 
 // We define the function to switch the surname with the first name
 // only for one type
